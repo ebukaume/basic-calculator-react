f5342e262c16a9215c2cf07b012d2e3b561eec97
Ebuka Umeokonkwo <ebukaume@gmail.com>
2019-11-03 16:32:23 +0100

    extract widths to constants in Button.jsx

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,9 +2,11 @@ import React from 'react';
 
 export default function Button({ name, color, wide }) {
   const DEFAULT_COLOR = 'orange'
+  const NORMAL = '25%'
+  const WIDE = '50%'
   const styles = {};
 
-  styles.width = wide ? '50%' : '25%';
+  styles.width = wide ? WIDE : NORMAL;
   styles.backgroundColor = color === undefined ? DEFAULT_COLOR : color;
 
   return (
