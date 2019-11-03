372cd70525d74082d674d7679e4fa6cc112c7a4f
Ebuka Umeokonkwo <ebukaume@gmail.com>
2019-11-03 18:16:57 +0100

    refactor style object in Button.jsx

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -4,10 +4,11 @@ export default function Button({ name, color, wide }) {
   const DEFAULT_COLOR = 'orange'
   const NORMAL = '25%'
   const WIDE = '50%'
-  const styles = {};
-
-  styles.width = wide ? WIDE : NORMAL;
-  styles.backgroundColor = color === undefined ? DEFAULT_COLOR : color;
+  
+  const styles = {
+    width: wide ? WIDE : NORMAL,
+    backgroundColor: color ? color : DEFAULT_COLOR
+  }
 
   return (
     <button style={ styles }>
